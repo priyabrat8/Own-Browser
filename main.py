19341c6c0898e2b9d127ef2be3e1b19e4781e8e1
priyabrat <priyabratswain888@gmail.com>
2024-12-08 22:02:45 +0530

    Replace emojis with images

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 import sys
 
 class MainWindow(QMainWindow):
@@ -15,37 +16,39 @@ class MainWindow(QMainWindow):
         navbar = QToolBar()
         self.addToolBar(navbar)
 
-        home_btn = QAction('🏠', self)
+        home_icon = QIcon("./img/house.png")
+        home_btn = QAction(home_icon,'Go to Home', self)
         home_btn.triggered.connect(self.navigate_home)
         navbar.addAction(home_btn)
 
-        back_btn = QAction('👈', self)
+        back_icon = QIcon("./img/back.png")
+        back_btn = QAction(back_icon,'Back', self)
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
 
-        forward_btn = QAction('👉', self)
+        reload_icon = QIcon("./img/reload.png")
+        reload_btn = QAction(reload_icon,'reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        forward_icon = QIcon("./img/forward.png")
+        forward_btn = QAction(forward_icon,'Forward', self)
         forward_btn.triggered.connect(self.browser.forward)
         navbar.addAction(forward_btn)
 
-        reload_btn = QAction('🔄️', self)
-        reload_btn.triggered.connect(self.browser.reload)
-        navbar.addAction(reload_btn)
+        
 
         # self.url_bar = QLineEdit()
         # self.url_bar.returnPressed.connect(self.navigate_to_url)
         # navbar.addWidget(self.url_bar)
 
         
-
     def navigate_home(self):
         self.browser.setUrl(QUrl('https://google.com'))
     
     # def navigate_to_url(self):
     #     url = self.url_bar.text()
     #     self.browser.setUrl(QUrl(url))
-
-
-
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName("Hi")
