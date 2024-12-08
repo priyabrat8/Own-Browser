8b88ccb92c420dc1d2726b3556a3a8d8f9e64d38
priyabrat <priyabratswain888@gmail.com>
2024-12-08 22:49:44 +0530

    Add MainWindow Icon

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.icon_path="./img/"
+
         self.browser = QWebEngineView()
+        self.setWindowIcon(QIcon(self.icon_path+"flashbrowse.png"))
         self.browser.setUrl(QUrl('https://google.com'))
         self.setCentralWidget(self.browser)
         self.showMaximized()
@@ -16,7 +19,7 @@ class MainWindow(QMainWindow):
         navbar = QToolBar()
         self.addToolBar(navbar)
 
-        home_icon = QIcon("./img/house.png")
+        home_icon = QIcon(self.icon_path+"house.png")
         home_btn = QAction(home_icon,'Go to Home', self)
         home_btn.triggered.connect(self.navigate_home)
         navbar.addAction(home_btn)
@@ -25,17 +28,17 @@ class MainWindow(QMainWindow):
         spacer.setFixedWidth(25)
         navbar.addWidget(spacer)
 
-        back_icon = QIcon("./img/back.png")
+        back_icon = QIcon(self.icon_path+"back.png")
         back_btn = QAction(back_icon,'Back', self)
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
 
-        reload_icon = QIcon("./img/reload.png")
+        reload_icon = QIcon(self.icon_path+"reload.png")
         reload_btn = QAction(reload_icon,'Reload', self)
         reload_btn.triggered.connect(self.browser.reload)
         navbar.addAction(reload_btn)
 
-        forward_icon = QIcon("./img/forward.png")
+        forward_icon = QIcon(self.icon_path+"forward.png")
         forward_btn = QAction(forward_icon,'Forward', self)
         forward_btn.triggered.connect(self.browser.forward)
         navbar.addAction(forward_btn)
@@ -45,8 +48,7 @@ class MainWindow(QMainWindow):
         # self.url_bar = QLineEdit()
         # self.url_bar.returnPressed.connect(self.navigate_to_url)
         # navbar.addWidget(self.url_bar)
-
-        
+     
     def navigate_home(self):
         self.browser.setUrl(QUrl('https://google.com'))
     
@@ -55,7 +57,7 @@ class MainWindow(QMainWindow):
     #     self.browser.setUrl(QUrl(url))
 
 app = QApplication(sys.argv)
-QApplication.setApplicationName("Hi")
+QApplication.setApplicationName("Flash Browse")
 window = MainWindow()
 
 app.exec_()
